ccf5be2db95551a63e47a216059ee1b016760c2d
Cody Sigvartson <cosig03@gmail.com>
2018-09-20 04:04:05 -0400

    Ethernet frame building, packing, unpacking
    Implemented the construction of ethernet header + payload, serialization, and de-serialization. Sendto() keeps returning -1 so need to look into why the send is failing.

--- 455_proj2.c
+++ 455_proj2.c
@@ -12,35 +12,103 @@
 #define SEND 0
 #define RECV 1
 
-void send_message(){
+void send_message(char if_name[], struct sockaddr_ll sk_addr, char hw_addr[], char payload[]){
+	// create socket
+	int sockfd = -1;
+	if((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)))<0){
+		perror("socket() failed!");
+	}
 
-	//Do something here
+	printf("Sending with sockfd: %d\n",sockfd);
 
+	// connect to interface name
+	struct ifreq if_hwaddr;
+	memset(&if_hwaddr,0,sizeof(struct ifreq));
+	strncpy(if_hwaddr.ifr_name, if_name, IFNAMSIZ-1);
+	if(ioctl(sockfd, SIOCGIFHWADDR, &if_hwaddr) < 0){
+		perror("SIOCGIFHWADDR");
+	}
+
+	struct ether_header frame;
+	memset(&frame,0,sizeof(struct ether_header));
+	strncpy(frame.ether_dhost, hw_addr, 6);
+	strncpy(frame.ether_shost, if_hwaddr.ifr_hwaddr.sa_data, 6);
+	frame.ether_type = htons(ETH_P_IP);
+
+
+	struct ifreq if_idx;
+	memset(&if_idx,0,sizeof(struct ifreq));
+	strncpy(if_idx.ifr_name, if_name, IFNAMSIZ-1);
+	if(ioctl(sockfd, SIOCGIFINDEX, &if_idx) < 0){
+		perror("SIOCGIFINDEX");
+	}
+
+	// pack frame header
+	unsigned char buff[BUF_SIZ];
+	memcpy(buff, &frame, sizeof(struct ether_header));
+	// include payload
+	memcpy(&buff[sizeof(struct ether_header)], payload, strlen(payload)+1);
+	printf("Payload: %s\n", payload);
+	printf("Sending frame: [%X][%X][%X][%X][%X][%X]|[%X][%X][%X][%X][%X][%X]|[%X][%X]|%c%c\n", buff[0],buff[1],buff[2],buff[3],buff[4],buff[5],buff[6],buff[7],buff[8],buff[9],buff[10],buff[11],buff[12],buff[13],buff[14],buff[15]);
+
+	sk_addr.sll_ifindex = if_idx.ifr_ifindex;
+	sk_addr.sll_halen = ETH_ALEN;
+	int byteSent = sendto(sockfd, buff, sizeof(buff), 0, (struct sockaddr*)&sk_addr, sizeof(struct sockaddr_ll));
+	printf("%d bytes sent!\n", byteSent);
 }
 
-void recv_message(){
+void recv_message(char if_name[], struct sockaddr_ll sk_addr){
+	int sockfd = -1;
+	if((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)))<0){
+		perror("socket() failed!");
+	}
 
-	//Do something here
+	struct ifreq if_idx;
+	memset(&if_idx,0,sizeof(struct ifreq));
+	strncpy(if_idx.ifr_name, if_name, IFNAMSIZ-1);
+	if(ioctl(sockfd, SIOCGIFINDEX, &if_idx) < 0){
+		perror("SIOCGIFINDEX");
+	}
 
+	if(ioctl(sockfd, SIOCGIFHWADDR, &if_idx) < 0){
+		perror("SIOCGIFINDEX");
+	}
+
+	unsigned char buff[BUF_SIZ];
+	int sk_addr_size = sizeof(struct sockaddr_ll);
+	int recvLen = recvfrom(sockfd, buff, BUF_SIZ, 0 , (struct sockaddr*)&sk_addr, &sk_addr_size);
+	
+	printf("%d bytes received!\n", recvLen);
+
+	unsigned char src_mac[6];
+	memcpy(src_mac, &buff[6], 6);
+	
+	unsigned char payload[BUF_SIZ-14];
+	memcpy(payload, &buff[14], BUF_SIZ-14);
+
+	printf("Message: %c %c\n",payload[0], payload[1]);
+	printf("Message from MAC: %s\n", src_mac);
 }
 
 int main(int argc, char *argv[])
 {
+	printf("Inside main\n");
 	int mode;
+	char buff[BUF_SIZ];
 	char hw_addr[6];
 	char interfaceName[IFNAMSIZ];
-	char buf[BUF_SIZ];
-	memset(buf, 0, BUF_SIZ);
+	memset(buff, 0, BUF_SIZ);
 	
 	int correct=0;
 	if (argc > 1){
 		if(strncmp(argv[1],"Send", 4)==0){
+			printf("Preparing to send a frame!\n");
 			if (argc == 5){
 				mode=SEND; 
 				sscanf(argv[3], "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &hw_addr[0], &hw_addr[1], &hw_addr[2], &hw_addr[3], &hw_addr[4], &hw_addr[5]);
-				strncpy(buf, argv[4], BUF_SIZ);
+				strncpy(buff, argv[4], BUF_SIZ);
+				printf("Sending payload: %s\n", buff);
 				correct=1;
-				printf("  buf: %s\n", buf);
 			}
 		}
 		else if(strncmp(argv[1],"Recv", 4)==0){
@@ -57,13 +125,15 @@ int main(int argc, char *argv[])
 		exit(1);
 	 }
 
-	//Do something here
+	struct sockaddr_ll sk_addr;
+	memset(&sk_addr, 0, sizeof(struct sockaddr_ll));
+
 
 	if(mode == SEND){
-		send_message();
+		send_message(interfaceName, sk_addr, hw_addr, buff);
 	}
 	else if (mode == RECV){
-		recv_message();
+		recv_message(interfaceName, sk_addr);
 	}
 
 	return 0;
